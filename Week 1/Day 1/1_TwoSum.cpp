e3e3319605198c9fcf2aadc7591dbbac965c75c2
Shivam Sangal <shivamsangal94102@gmail.com>
2026-06-02 12:31:14 +0530

    Week 1 Day 1 DSA

---
+++ Week 1/Day 1/1_TwoSum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int more = target - nums[i];
+            if (mp.find(more) != mp.end()) {
+                return {mp[more], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
---
+++ Week 1/Day 1/3_Best-Time-To-Buy-Sell-Stock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int bestBuy = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] > bestBuy) {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
