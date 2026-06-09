2cde8fbef1c9e4f448b12daa7f418e81ef2b1c84
Shivam Sangal <shivamsangal94102@gmail.com>
2026-06-09 11:11:58 +0530

    Week 1 Day 5 DSA

---
+++ Week 1/Day 5/3_Longest_Common_Prefix.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans=strs[0];
+        for(int i=1;i<strs.size();i++){
+            while(strs[i].find(ans)!=0){
+                ans.pop_back();
+                if(ans.empty()){
+                    return "";
+                }
+            }
+        }
+        return ans;
+    }
+};
